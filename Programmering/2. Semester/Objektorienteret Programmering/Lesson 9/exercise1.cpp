14aaf37543c66f1e12d3b712e1c4611c960328f5
Robert Klastrup <AU785714@uni.au.dk>
2026-05-21 22:23:04 +0200

    Tilføjelse af kommentare

--- Programmering/2. Semester/Objektorienteret Programmering/Lesson 9/exercise1.cpp
+++ Programmering/2. Semester/Objektorienteret Programmering/Lesson 9/exercise1.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+// Person-klasse med ssn og navn:
 class Person {
     friend ostream& operator<<(ostream& os, const Person& p);
 
@@ -9,16 +10,21 @@ class Person {
         int ssn_;
         string name_;
     public:
+        // sætter ssn og navn:
         Person(int s, const string& n) : ssn_(s), name_(n) {}
 };
+
+// overload: giver << ny betydning for Person (print ssn + navn):
 ostream& operator<<(ostream& os, const Person& p) {
     os << "Ssn:" << p.ssn_ << " Name:" << p.name_;
     return os;
 }
 
 int main() {
+    // vector med 10 ens Person-objekter:
     vector<Person> persons(10, Person(1, "John Doe"));
 
+    // printer alle personer med index-loop:
     for (int i = 0; i < persons.size(); i++) {
         cout << persons[i];
         if (i < persons.size() - 1) cout << "\n";
--- Programmering/2. Semester/Objektorienteret Programmering/Lesson 9/exercise3.cpp
+++ Programmering/2. Semester/Objektorienteret Programmering/Lesson 9/exercise3.cpp
@@ -7,19 +7,23 @@ using namespace std;
 #include <deque>
 using namespace std;
 
+// Person-klasse med copy-konstruktør:
 class Person{
     friend ostream &operator<<(ostream &, const Person &);
 
     public:
+        // sætter ssn og navn:
         Person (int ssn, const string &name) : ssn_(ssn), name_(name) {}
 
+        // kopierer eksisterende Person-objekt:
         Person (const Person &to_copy) : ssn_(to_copy.ssn_), name_(to_copy.name_){
             cout << "Copy Ctor called" << '\n';
         }
         int ssn_;
-        string name_;   
+        string name_;
 };
 
+// overload: giver << ny betydning for Person (print ssn + navn):
 ostream &operator << (ostream &os, const Person &Person){
     os << "Ssn: " << Person.ssn_ << " Name: " << Person.name_;
     return os;
@@ -27,13 +31,15 @@ ostream &operator << (ostream &os, const Person &Person){
 
 
 int main() {
-   
+    // deque: vector med hurtig indsæt begge ender:
     deque<Person> persons;
 
+    // indsætter 3 personer forrest (nyeste kommer først):
     persons.emplace_front(1, "Jeff Doe");
     persons.emplace_front(2, "Jeff Doe");
     persons.emplace_front(3, "Jeff Doe");
 
+    // printer alle personer i deque:
     for (const auto& p : persons) {
         cout << p << '\n';
     }
